37d397feef1b2517c975f7681cf6ed8b622b2295
S-S-M <buddha566@gmail.com>
2023-03-17 14:11:26 -0400

    updated script to work with default folder structure

--- bash_scripts/get_counts.sh
+++ bash_scripts/get_counts.sh
@@ -1,1 +1,1 @@
-ls *.bam | parallel "samtools view {} | cut -f 3 | sort | uniq -c | awk '{t = \$ 1; \$ 1 = \$ 2; \$ 2 = t; print}' > {}.counts.txt"
+ls **/*.bam | parallel "samtools view {} | cut -f 3 | sort | uniq -c | awk '{t = \$ 1; \$ 1 = \$ 2; \$ 2 = t; print}' > {}.counts.txt"
